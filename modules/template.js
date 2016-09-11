233cb93ad11e0366365055d60ed926c70e981c36
canmom <bryn.dickinson@gmail.com>
2016-09-11 05:47:31 +0100

    Slight documentation improvement on the generic template for modules.

--- modules/template.js
+++ modules/template.js
@@ -1,9 +1,12 @@
 //template of a module for the bot, showing the basic structure
 
 Command = require('../command');
+//uncomment this line if you need access to the bot object:
+//Bot = require('../bot');
 
 var commands = [];
 
+//basic syntax for adding a new command
 commands.push(
 	new Command.Command(
 		"command",

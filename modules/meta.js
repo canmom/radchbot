7a0c330cc42575ad09985991fe5269b511dbf12d
canmom <bryn.dickinson@gmail.com>
2016-09-20 23:51:28 +0100

    Created a way for users to add simple temporary commands during runtime.

--- modules/meta.js
+++ modules/meta.js
@@ -8,7 +8,6 @@ commands = [];
 function repeat(args,message) {
 	var iterations = parseInt(args.split(' ',1),10);
 	var repeatedInstruction = args.slice(args.indexOf(' ')+1);
-	var reply = `OK, I'm running '${repeatedInstruction}' ${iterations} times.\n`
 
 	// if the user has written !command instead of just command, remove the !
 	if (repeatedInstruction.startsWith('!')) {
@@ -27,6 +26,7 @@ function repeat(args,message) {
 
 	//if a command has been identified, execute it N times and append the results to message
 	if (repeatedCommand) {
+		var reply = `OK, I'm running '${repeatedInstruction}' ${iterations} times.\n`
 		var repeatedCommandArgs = repeatedInstruction.slice(repeatedCommand.command.length);
 		for (var i = 0; i<iterations; i++) {
 			reply += ">" + repeatedInstruction + "\n";
@@ -37,11 +37,49 @@ function repeat(args,message) {
 	return reply;
 }
 
+function addCommand(args,message) {
+	var newCommand = args.split(' ',1);
+	var newCommandMessage = args.slice(args.indexOf(' ')+1);
+
+	var commandAlreadyExists = false;
+
+	for (command of Bot.commands) {
+		if (command.command.startsWith(newCommand)) {
+			commandAlreadyExists = true;
+		}
+	};
+
+	if (commandAlreadyExists) {
+		return "Sorry, but there's already a command by that name! Try calling it something else.";
+	}
+	else {
+		Bot.commands.push(
+			new Command.Command(
+				newCommand,
+				"Temporary command added by " + message.guild.member(message.author),
+				function () {
+					return newCommandMessage;
+				}
+			)
+		);
+
+		return "OK, when someone says !" + newCommand + ", I will say '" + newCommandMessage + "'!";
+	}
+}
+
 commands.push(
 	new Command.Command(
 		"repeat ",
 		"Repeat a command multiple times. Syntax: '!repeat <number> <command> <arguments>' e.g. '!repeat 6 roll 4d6b3'",
 		repeat
+	)
+)
+
+commands.push(
+	new Command.Command(
+		"newcommand ",
+		"Add a simple, temporary command that returns a message when called. Syntax: !addcommand <command> <message>. Does not persist through bot shutdown!",
+		addCommand
 	)
 )
 
